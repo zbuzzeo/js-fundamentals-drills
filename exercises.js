e56099c7d117e363a06fa57f6114e168f0e5fd86
lorecrafting <raymond.n.luong@gmail.com>
2017-04-21 15:57:43 -1000

    exercise prompt comments skeleton

---
+++ exercises.js
@@ -0,0 +1,364 @@
+/* #doubleArray
+ *
+ * Takes in an array of numbers and returns an array with each element doubled
+ *
+ * @param {Array} // [1,2,3]
+ * @return {Array} // [2,4,6]
+ */
+var doubleArray;
+
+/* #sumArrays
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var sumArrays;
+
+/* #stringCount
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var stringCount;
+
+/* #arrayLength
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var arrayLength;
+
+/* #countAll
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var countAll;
+
+/* #countStrings
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var countStrings;
+
+/* #countAllStrings
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var countAllStrings;
+
+/* #convertToArray
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var convertToArray;
+
+/* #objectSize
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var objectSize;
+
+/* #createZeroFilledArray
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var createZeroFilledArray;
+
+/* #poppedArray
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var poppedArray;
+
+/* #splitString
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var splitString;
+
+/* #lengthOfLast
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var lengthOfLast;
+
+/* #sumBelowTen
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var sumBelowTen;
+
+/* #moreThanTenLetters
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var moreThanTenLetters;
+
+/* #multiplyAll
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var multiplyAll;
+
+/* #getKeys
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var getKeys;
+
+/* #sumAllPositive
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var sumAllPositive;
+
+/* #stringCountBelowThree
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var stringCountBelowThree;
+
+/* #countObjects
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var countObjects;
+
+/* #getObjectKeys
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var getObjectKeys;
+
+/* #getObjectValues
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var getObjectValues;
+
+/* #makeObject
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var makeObject;
+
+/* #makeObjectReverse
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var makeObjectReverse;
+
+/* #tupleToObject
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var tupleToObject;
+
+/* #tupleToObjectReverse
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var tupleToObjectReverse;
+
+/* #stringToKeys
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var stringToKeys;
+
+/* #getValues
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var getValues;
+
+/* #getKeys
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var getKeys;
+
+/* #objectToArray
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var objectToArray;
+
+/* #arrayToObject
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var arrayToObject;
+
+/* #arraysToObject
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var arraysToObject;
+
+/* #objectsToTuples
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var objectsToTuples;
+
+/* #mapArrayValues
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var mapArrayValues;
+
+/* #mapStringCounts
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var mapStringCounts;
+
+/* #arrayToObjectNums
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var arrayToObjectNums;
+
+/* #stringToKeys
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var stringToKeys;
+
+/* #charCountMap
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var charCountMap;
+
+/* #frequencyMap
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var frequencyMap;
+
+/* #tupleToObject
+ *
+ *
+ * @param {String}
+ * @return {Bool}
+ */
+var tupleToObject;
+
+
+module.exports = {
+  doubleArray: null,
+  sumArrays: null, 
+  stringCount: null,
+  arrayLength: null,
+  countAll: null,
+  countStrings: null,
+  countAllStrings: null,
+  convertToArray: null,
+  objectSize: null,
+  createZeroFilledArray: null,
+  poppedArray: null,
+  splitString: null,
+  lengthOfLast: null,
+  sumBelowTen: null,
+  moreThanTenLetters: null,
+  multiplyAll: null,
+  getKeys: null,
+  sumAllPositive: null,
+  stringCountBelowThree: null,
+  countObjects: null,
+  getObjectKeys: null,
+  getObjectValues: null,
+  makeObject: null,
+  makeObjectReverse: null,
+  tupleToObject: null,
+  tupleToObjectReverse: null,
+  stringToKeys: null,
+  getValues: null,
+  getKeys: null,
+  objectToArray: null,
+  arrayToObject: null,
+  arraysToObject: null,
+  objectsToTuples: null,
+  mapArrayValues: null,
+  mapStringCounts: null,
+  arrayToObjectNums: null,
+  stringToKeys: null,
+  charCountMap: null,
+  frequencyMap: null,
+  tupleToObject: null
+}
